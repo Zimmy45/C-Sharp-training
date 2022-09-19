d6de7cfac47f197de1cbdd0d8d2aafb39b1687af
Zimmy45 <zimmy45@mail.ru>
2022-09-19 22:19:12 +0300

    Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите два числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numberA + numberB);
