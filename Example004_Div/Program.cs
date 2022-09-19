9768a0230ed29fccec9c68d439cb059ebcf6268b
Zimmy45 <zimmy45@mail.ru>
2022-09-19 22:26:54 +0300

    Example 004 Division

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите два числа");
+double numberA = Convert.ToInt32(Console.ReadLine());
+double numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numberA / numberB);
