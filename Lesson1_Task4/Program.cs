b683af417403a4901666d712dae76ba9b4a10259
Zimmy45 <zimmy45@mail.ru>
2022-09-23 20:20:28 +0300

    Lesson1_Task4

---
+++ Lesson1_Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите три числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max=", a);
+}
+else
+{
+Console.WriteLine("max=" + b);    
+}
