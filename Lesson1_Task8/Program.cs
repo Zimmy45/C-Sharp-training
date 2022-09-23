25f683856b12bdba345cdd1ee2c5e3e33581b228
Zimmy45 <zimmy45@mail.ru>
2022-09-23 21:06:14 +0300

    Lesson1_Task8

--- Lesson1_Task8/Program.cs
+++ Lesson1_Task8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int x = 2;
+
+while (x <= N)
+{
+    Console.Write(x);
+    Console.Write(", ");
+    x = x +2;
+    }
