fd6f3b9347aaec919d9a6dcb70b87afe677e2941
Zimmy45 <zimmy45@mail.ru>
2022-09-25 09:20:55 +0300

    Lesson2_Task10

--- Lesson2_Task10/Program.cs
+++ Lesson2_Task10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 100 || x > 999)
+Console.WriteLine("Вы уверены, что оно трехзначное? Попробуйте еще раз.");
+else
+{
+    int y = (x % 100) / 10;
+    Console.WriteLine("Вторая цифра - " + y);
+}
