c14cd7e637acd6b8bd8182b7030963fb54e40354
Zimmy45 <zimmy45@mail.ru>
2022-09-25 09:49:50 +0300

    Lesson2_Task15

--- Lesson2_Task15/Program.cs
+++ Lesson2_Task15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите день недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x > 7)
+Console.WriteLine("Нет столько дней в неделе. Попробуйте еще раз.");
+else if (x == 6 || x == 7)
+{
+    Console.WriteLine("Это выходной");
+}
+else
+Console.WriteLine("Это не выходной");
