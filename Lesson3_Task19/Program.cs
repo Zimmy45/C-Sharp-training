cf5a1ce27d4d41eb2a909639a63221e816b6af92
Zimmy45 <zimmy45@mail.ru>
2022-10-01 09:38:45 +0300

    Lesson3_Task19

---
+++ Lesson3_Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a > 99999 || a < 1000)
+{
+    Console.Write("Число не похоже на пятизначное. Попробуйте еще раз: ");
+    a = Convert.ToInt32(Console.ReadLine());
+};
+
+if (a / 10000 == a % 10 && a / 1000 % 10 == a % 100 / 10)
+
+{
+    Console.Write("Это палиндром");
+}
+else
+    Console.Write("Нет, это не палиндром");
