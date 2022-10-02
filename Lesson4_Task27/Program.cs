1f99b1a7fdf9313fe9c31a98945c84c3a8d4a0f7
Zimmy45 <zimmy45@mail.ru>
2022-10-02 09:46:49 +0300

    Lesson4_Task27

---
+++ Lesson4_Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+string? l = Convert.ToString(a);
+
+for (int i = 1; i <= l.Length; i++)
+{
+    sum = sum + a % 10;
+    a = a / 10;     
+};
+Console.WriteLine(sum);
