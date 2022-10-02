d40cb9068fdfe421a3ca300413b829e816848f4f
Zimmy45 <zimmy45@mail.ru>
2022-10-02 09:13:16 +0300

    Lesson4_task25

---
+++ Lesson4_Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("В какую степень его нужно возвести: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = 1;
+for (int i = 1; i <= b; i++)
+{
+    step = step * a;    
+};
+Console.WriteLine(step);
