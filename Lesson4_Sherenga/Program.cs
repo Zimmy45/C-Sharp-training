48cd6d9010cd415577fa0bb1005bf86915bc5e06
Zimmy45 <zimmy45@mail.ru>
2022-10-04 01:11:39 +0300

    Lesson4_Sherenga

--- Lesson4_Sherenga/Program.cs
+++ Lesson4_Sherenga/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Сколько учеников в Петином классе (не считая Петю)? ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] A = new int[N];
+Console.WriteLine("Введите рост учеников:");
+for (int i = 0; i < A.Length; i++)
+    {
+        Console.Write($"ученик {i+1}: ");
+    A[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+Console.Write("Какой рост Пети? ");
+int X = Convert.ToInt32(Console.ReadLine());
+// Перебираем шеренгу и сравниваем с Петей
+int pos = 0;
+for (int i = 0; i < A.Length; i++)
+    {
+        if (X <= A[i]) pos = i+1;
+    }
+Console.WriteLine($"Петя должен занять {pos+1}-ую позицию.");
+// Хотел вывести массив, подменив в нем рост Пети его именем
+// но не нашел как заменить целочисленное значение строковой величиной.
+// Это можно как-нибудь реализовать типа:
+// A[pos] = string "Петя";
+// Console.WriteLine("[ " + string.Join(" | ", A) + " ]");
