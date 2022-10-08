aaf1b8d6ed60089d600c60fd6da3cc2a5a1a9965
Zimmy45 <zimmy45@mail.ru>
2022-10-08 11:15:57 +0300

    Task51

---
+++ train/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int sum = 0;
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + " \t");
+            if (i == j) sum = sum + matrix[i, j];
+        }
+    Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine($"Сумма главной диагонали {sum}");
