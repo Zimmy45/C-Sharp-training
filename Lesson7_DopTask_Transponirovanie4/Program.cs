076c03d7b1f568d35d3d49a79a43d50baf355a41
Zimmy45 <zimmy45@mail.ru>
2022-10-11 02:19:06 +0300

    Lesson7_DopTask_Transponirovanie4

--- Lesson7_DopTask_Transponirovanie4/Program.cs
+++ Lesson7_DopTask_Transponirovanie4/Program.cs
@@ -1,16 +1,17 @@
 ﻿Console.Clear();
 
-void InputMatrix(double[,] matrix)
+void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+            matrix[i, j] = new Random().Next(0, 101);
             Console.Write(matrix[i, j] + " \t");
         }
         Console.WriteLine();
     }
+
 }
 
 
@@ -18,6 +19,17 @@ Console.Write("Введите кол-во строк m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите кол-во столбцов n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] matrix = new double[m, n];
+int[,] matrix = new int[m, n];
 InputMatrix(matrix);
-
+string sep = Convert.ToString("--"+"\t");
+Console.WriteLine(string.Concat(Enumerable.Repeat(sep, n)));
+int[,] matrixResult = new int[m, n];
+for (int i = 0; i < matrixResult.GetLength(0); i++)
+{
+    for (int j = 0; j < matrixResult.GetLength(1); j++)
+    {
+        matrixResult[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+        Console.Write(matrixResult[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
