347cbe1da54f835c70de4535ce3039887f6bdbd5
Zimmy45 <zimmy45@mail.ru>
2022-10-14 17:03:10 +0300

    Lesson8_Task56

--- Lesson8_Task56/Program.cs
+++ Lesson8_Task56/Program.cs
@@ -1,26 +1,28 @@
 ﻿Console.Clear();
-void InputMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < matrix.GetLength(1)-1; j++)
-        {
-            matrix[i, j] = Convert.ToInt32(new Random().Next(1, 10));
-            Console.Write(matrix[i, j] + " \t");
-            sum = sum + matrix[i, j];
-        }
-        matrix[i, j+1] = sum;
-        Console.WriteLine();
-    }
-}
-
-
 Console.Write("Введите кол-во строк i: ");
 int i = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите кол-во столбцов j: ");
 int j = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[i, j+1];
-InputMatrix(matrix);
-Console.WriteLine();
-
+int[,] matrix = new int[i, j];
+int minSum = 0;
+int row = 1;
+for (int m = 0; m < matrix.GetLength(0); m++)
+{
+    int sum = 0;
+    for (int n = 0; n < matrix.GetLength(1); n++)
+    {
+        matrix[m, n] = Convert.ToInt32(new Random().Next(1, 10));
+        Console.Write(matrix[m, n] + " \t");
+        sum = sum + matrix[m, n]; // суммирую элементы строки
+        if (m == 0 && n == matrix.GetLength(1)-1) minSum = sum;// присваиваю переменной minSum сумму эл-тов первой строки
+    }
+        //Console.Write($"||{sum}"); вывод суммы по строке
+        if (sum < minSum)
+    {
+        minSum = sum;
+        row = m+1;//запоминаю строку
+    }
+Console.WriteLine();        
+}
+ Console.WriteLine();
+Console.WriteLine($"{row} строка");
