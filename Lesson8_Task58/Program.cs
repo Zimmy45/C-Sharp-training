a56899c89ad5dd9e7b979874d6c25dea6f0d8406
Zimmy45 <zimmy45@mail.ru>
2022-10-15 09:59:37 +0300

    Lesson8_Task58

--- Lesson8_Task58/Program.cs
+++ Lesson8_Task58/Program.cs
@@ -1,23 +1,38 @@
 ﻿Console.Clear();
-Console.Write("Введите кол-во строк i: ");
+Console.Write("Введите кол-во строк первой матрицы (оно же - количество столбцов второй матрицы): ");
 int i = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов j: ");
+Console.Write("Введите кол-во столбцов первой матрицы (оно же - количество строк второй матрицы): ");
 int j = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[i, j];
-int minSum = 0;
-int row = 1;
-for (int m = 0; m < matrix.GetLength(0); m++)
+int[,] matrix1 = new int[i, j];
+int[,] matrix2 = new int[j, i];
+Console.WriteLine("Массив 1:");
+InputMatrix(matrix1);
+Console.WriteLine("Массив 2:");
+InputMatrix(matrix2);
+Console.WriteLine("Массив, полученный перемножением первых двух:");
+int[,] matrix3 = new int[i, j];
+for (int m = 0; m < matrix1.GetLength(0); m++)
 {
     int sum = 0;
-    for (int n = 0; n < matrix.GetLength(1); n++)
+    for (int n = 0; n < matrix1.GetLength(1); n++)
     {
-        matrix[m, n] = Convert.ToInt32(new Random().Next(1, 10));
-        Console.Write(matrix[m, n] + " \t");
-        sum = sum + matrix[m, n]; // суммирую элементы строки
-        
-        
+        matrix3[m, n] = sum + matrix1[m,n]*matrix2[n,m];
+        Console.Write(matrix3[m, n] + " \t");               
     }
 Console.WriteLine();        
 }
  Console.WriteLine();
-Console.WriteLine($"{row} строка");
+
+void InputMatrix(int[,] matrix)
+{
+for (int m = 0; m < matrix.GetLength(0); m++)
+{
+    for (int n = 0; n < matrix.GetLength(1); n++)
+    {
+        matrix[m, n] = Convert.ToInt32(new Random().Next(1, 10));
+        Console.Write(matrix[m, n] + " \t");               
+    }
+Console.WriteLine();        
+}
+ Console.WriteLine();
+}
