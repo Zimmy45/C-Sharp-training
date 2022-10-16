81c162428ce5d4931cd8554269276edf6441d501
Zimmy45 <zimmy45@mail.ru>
2022-10-16 04:00:15 +0300

    Lesson8_Task60

---
+++ Lesson8_Task60/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+void InputMatrix(int[,,] matrix)
+{
+    for (int x = 0; x < matrix.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[x, y, z] = Convert.ToInt32(new Random().Next(10, 100));
+                Console.Write(matrix[x, y, z] + " (" + x + "," + y + "," + z + ")" + " \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите размер матрицы X: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер матрицы Y: ");
+int j = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер матрицы Z: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = new int[i, j, k];
+Console.WriteLine();
+InputMatrix(matrix);
+
+
