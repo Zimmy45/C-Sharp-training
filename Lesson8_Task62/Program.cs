a962a0c99be2c7f02d3ae40d9b78ce0c6b67286e
Zimmy45 <zimmy45@mail.ru>
2022-10-21 09:57:11 +0300

    Lesson9_Task66

---
+++ Lesson8_Task62/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+void InputMatrix(int[,] matrix)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToInt32(new Random().Next(1, 10));
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+void OutputMatrix(int[,] matrix)
+{
+    for (int m = 0; m < matrix.GetLength(0); m++)
+    {
+        for (int n = 0; n < matrix.GetLength(1); n++)
+        {
+            Console.Write(matrix[m, n] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк i: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов j: ");
+int j = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[i, j];
+InputMatrix(matrix);
+Console.WriteLine();
+OutputMatrix(matrix);
---
+++ Lesson9_Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿string Print(int m, int n, int sum)
+{
+
+    if (n == m)
+        return $"{sum + m} ";
+    return Print(m+1, n, sum + m);
+}
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.WriteLine(Print(Math.Min(n, m), Math.Max(n, m), sum));
