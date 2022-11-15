f31a579a879511ad6aa500d68b60b73520576c41
Zimmy45 <zimmy45@mail.ru>
2022-11-15 12:10:54 +0300

    Itog

---
+++ Itog/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Сколько элементов будет в исходном массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[m];
+string[] array2 = new string[m];
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    array1[i] = Console.ReadLine();
+    if (array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+Console.WriteLine();
+Console.Write("[ ");
+for (int i = 0; i < count-1; i++)
+{
+        Console.Write(array2[i] + ", ");
+}
+Console.Write(array2[count-1]);
+Console.Write(" ]");
---
+++ train/Program.cs
@@ -0,0 +1,17 @@
+﻿// task 67
+
+
+int Print(int A, int B)
+{
+   if (B == 0)
+      return 1;
+   return Print(A, B - 1)*A;
+}
+
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Print(A,B));
